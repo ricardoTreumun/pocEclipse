ab385e31e2536ae14c650b3ede8a7432e2f56507
LAPTOP-7UC33770\Ricardo <ricardo.treumun@2brains.cl>
2017-05-30 16:34:07 -0400

    Se genera confglicto

--- src/cl/bci/poc/Inicio.java
+++ src/cl/bci/poc/Inicio.java
@@ -9,6 +9,8 @@ public class Inicio {
 		System.out.println("HolaMundo3");
 		System.out.println("HolaMundo4");
 		System.out.println("HolaMundo11111");
+		
+		System.out.println("HolaMundo2222222");
 	}
 
 }
