6abb34d4b17d8ae7d1579b690e488270bd4e99fa
LAPTOP-7UC33770\Ricardo <ricardo.treumun@2brains.cl>
2017-05-30 16:44:51 -0400

    Se agregan cambios en el TO

--- src/cl/bci/poc/to/ClienteTO.java
+++ src/cl/bci/poc/to/ClienteTO.java
@@ -5,7 +5,31 @@ public class ClienteTO {
 	private String nombre;
 	private String direccion;
 	private int edad;
-	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
 	/**
 	 * To String de la clase.
 	 */
@@ -20,7 +44,5 @@ public class ClienteTO {
 		builder.append("]");
 		return builder.toString();
 	}
-
-	
 
 }
