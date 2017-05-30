538014320ffbe34c7b630f56ed7cead04aee2bc0
LAPTOP-7UC33770\Ricardo <ricardo.treumun@2brains.cl>
2017-05-30 11:23:36 -0400

    Orden en el codigo.

--- src/cl/bci/poc/Inicio.java
+++ src/cl/bci/poc/Inicio.java
@@ -5,11 +5,8 @@ public class Inicio {
 	public static void main(String[] args) {
 		
 		System.out.println("HolaMundo");
-		
-		
-		System.out.println("ESGFDEGFD");
-		
-		System.out.println("Este es mi segundo cambio");
+		System.out.println("HolaMundo2");
+		System.out.println("HolaMundo3");
 	}
 
 }
