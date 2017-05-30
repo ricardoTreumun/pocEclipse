1d00bc8dc57ff7e805d80d9aa05222288d12b7f5
LAPTOP-7UC33770\Ricardo <ricardo.treumun@2brains.cl>
2017-05-30 11:14:16 -0400

    Primer cambio en la rama de eclipse

--- src/cl/bci/poc/Inicio.java
+++ src/cl/bci/poc/Inicio.java
@@ -5,11 +5,10 @@ public class Inicio {
 	public static void main(String[] args) {
 		
 		System.out.println("HolaMundo");
-		
-		
 		System.out.println("ESGFDEGFD");
-		
 		System.out.println("Este es mi segundo cambio");
+		
+		System.out.println("Este es un cambio en la rama de eclipse");
 	}
 
 }
