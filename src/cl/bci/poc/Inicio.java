120f987850dc18d01c79bd7f0fbbe669c3e290f9
LAPTOP-7UC33770\Ricardo <ricardo.treumun@2brains.cl>
2017-05-30 16:39:49 -0400

    svsdvdsf

--- src/cl/bci/poc/Inicio.java
+++ src/cl/bci/poc/Inicio.java
@@ -10,6 +10,8 @@ public class Inicio {
 		System.out.println("HolaMundo4");
 		System.out.println("HolaMundo11111");
 		
+		
+		
 		System.out.println("HolaMundo2222222           2222      1111             ");
 	}
 
